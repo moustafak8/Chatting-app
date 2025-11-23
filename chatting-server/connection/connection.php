28c174d52e6956bd00986c2783a979882b9efa12
moustafak8 <moustafaalkamel@gmail.com>
2025-11-23 10:28:41 +0200

    created Email/Password auth (login/register).

--- chatting-server/connection/connection.php
+++ chatting-server/connection/connection.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
 
-$connection = new mysqli ("localhost", "root","","");
+$connection = new mysqli ("localhost", "root","","chatting-app");
 
 if ($connection -> connect_error){
     die ("connection error:" . $connection-> connect_error);
