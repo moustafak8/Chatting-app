d27ec875d9998ebc8a2d8de79b726752c915774b
moustafak8 <moustafaalkamel@gmail.com>
2025-11-23 16:42:14 +0200

    added status controller and model

--- chatting-server/routes/apis.php
+++ chatting-server/routes/apis.php
@@ -8,5 +8,7 @@ $apis = [
     '/conversation/create' => ['controller' => 'conversationcontroller', 'method' => 'newconversation'],
     '/participant/create' => ['controller' => 'participantscontroller', 'method' => 'newparticipants'],
     '/message/create' => ['controller' => 'messagecontroller', 'method' => 'new_message'],
-    '/message' => ['controller' => 'messagecontroller', 'method' => 'get_message']
+    '/message' => ['controller' => 'messagecontroller', 'method' => 'get_message'],
+    '/status' => ['controller' => 'status_controller', 'method' => 'get_status'],
+    '/status/create' => ['controller' => 'status_controller', 'method' => 'new_status']
 ];
