b0498617ba0e2e709d5c2a0dd38a689d69e413a9
moustafak8 <moustafaalkamel@gmail.com>
2025-11-23 15:24:58 +0200

    added participants controller and model

--- chatting-server/routes/apis.php
+++ chatting-server/routes/apis.php
@@ -5,5 +5,6 @@ $apis = [
     '/users/update' => ['controller' => 'usercontroller', 'method' => 'updateuser'],
     '/users/delete' => ['controller' => 'usercontroller', 'method' => 'deleteuser'],
     '/users/login' => ['controller' => 'AuthController', 'method' => 'login'],
-    '/conversation/create' => ['controller' => 'conversationcontroller', 'method' => 'newconversation']
+    '/conversation/create' => ['controller' => 'conversationcontroller', 'method' => 'newconversation'],
+    '/participant/create' => ['controller' => 'participantscontroller', 'method' => 'newparticipants']
 ];
